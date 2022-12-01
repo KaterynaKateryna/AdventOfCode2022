a505a5d0a474617ea6a1a9235d3408e45d7c155c
KaterynaKateryna <tok.ar.tok@gmail.com>
2022-12-01 19:47:48 +0000

    day 1

---
+++ AdventOfCode2022.Day1/Program.cs
@@ -0,0 +1,8 @@
+﻿using AdventOfCode2022.Day1;
+
+CalorieCounting counter = new CalorieCounting();
+
+string[] input = await counter.GetInput();
+long answer1 = counter.GetMaxCalories(input);
+
+Console.WriteLine(answer1);
