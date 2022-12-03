583028873afabf16dc662a6db2214dfffaa1aa99
KaterynaKateryna <tok.ar.tok@gmail.com>
2022-12-03 12:48:30 +0100

    day 2 part 2

--- AdventOfCode2022.Day2/Program.cs
+++ AdventOfCode2022.Day2/Program.cs
@@ -2,8 +2,12 @@
 
 RockPaperScissors game = new RockPaperScissors();
 
-Round[] rounds = await game.GetInput();
-
 // part 1
-long score = game.GetScore(rounds);
-Console.WriteLine(score);
+RoundPart1[] rounds1 = await game.GetInputPart1();
+long score1 = game.GetScorePart1(rounds1);
+Console.WriteLine(score1);
+
+// part 2
+RoundPart2[] rounds2 = await game.GetInputPart2();
+long score2 = game.GetScorePart2(rounds2);
+Console.WriteLine(score2);
--- AdventOfCode2022.Day2/RockPaperScissors.cs
+++ AdventOfCode2022.Day2/RockPaperScissors.cs
@@ -2,58 +2,149 @@
 
 public class RockPaperScissors
 {
-    public async Task<Round[]> GetInput()
+    public async Task<RoundPart1[]> GetInputPart1()
+    {
+        Round[] result = await GetRawInput();
+        return result.Select(r => new RoundPart1((MoveType)r.Them, (MoveType)r.Me)).ToArray();
+    }
+
+    public async Task<RoundPart2[]> GetInputPart2()
+    {
+        Round[] result = await GetRawInput();
+        return result.Select(r => new RoundPart2((MoveType)r.Them, (Outcome)r.Me)).ToArray();
+    }
+
+    private async Task<Round[]> GetRawInput()
     {
         string[] lines = await File.ReadAllLinesAsync("input.txt");
         Round[] result = new Round[lines.Length];
         for (int i = 0; i < lines.Length; ++i)
         {
             string[] parts = lines[i].Split(' ');
-            MoveType them = (MoveType)(parts[0][0] - 'A'); 
-            MoveType me = (MoveType)(parts[1][0] - 'X');
+            int them = (parts[0][0] - 'A');
+            int me = (parts[1][0] - 'X');
             result[i] = new Round(them, me);
         }
         return result;
     }
 
-    public long GetScore(Round[] rounds)
+    public long GetScorePart1(RoundPart1[] rounds)
     {
         long score = 0;
 
-        foreach (Round round in rounds)
+        foreach (RoundPart1 round in rounds)
         {
-            score += (int)round.Me + 1;
-
-            if (round.Me == round.Them)
-            {
-                score += 3;
-            }
-            else if (MeWin(round))
-            {
-                score += 6;
-            }
+            score += GetScore(round.Me, GetOutcome(round.Them, round.Me));
         }
 
         return score;
     }
 
-    private bool MeWin(Round round)
+    public long GetScorePart2(RoundPart2[] rounds)
     {
-        if (round.Me == MoveType.Rock && round.Them == MoveType.Scissors)
-            return true;
-        if (round.Me == MoveType.Paper && round.Them == MoveType.Rock)
-            return true;
-        if (round.Me == MoveType.Scissors && round.Them == MoveType.Paper)
-            return true;
-        return false;
+        long score = 0;
+
+        foreach (RoundPart2 round in rounds)
+        {
+            score += GetScore(GetMeShape(round.Them, round.Me), round.Me);
+        }
+
+        return score;
+    }
+    private long GetScore(MoveType me, Outcome outcome)
+    {
+        long score = (int)me + 1;
+
+        if (outcome == Outcome.Draw)
+        {
+            score += 3;
+        }
+        if (outcome == Outcome.Win)
+        {
+            score += 6;
+        }
+
+        return score;
+    }
+
+    private Outcome GetOutcome(MoveType them, MoveType me)
+    {
+        if (them == me)
+            return Outcome.Draw;
+
+        if (me == MoveType.Rock && them == MoveType.Scissors)
+            return Outcome.Win;
+
+        if (me == MoveType.Paper && them == MoveType.Rock)
+            return Outcome.Win;
+
+        if (me == MoveType.Scissors && them == MoveType.Paper)
+            return Outcome.Win;
+
+        return Outcome.Lose;
+    }
+
+    private MoveType GetMeShape(MoveType them, Outcome me)
+    {
+        if (me == Outcome.Draw)
+            return them;
+
+        if (me == Outcome.Win)
+            return GetWinShape(them);
+
+        if (me == Outcome.Lose)
+            return GetLoseShape(them);
+
+        throw new ArgumentOutOfRangeException("outcome not recognised");
+    }
+
+    private MoveType GetWinShape(MoveType them)
+    {
+        switch (them)
+        {
+            case MoveType.Scissors:
+                return MoveType.Rock;
+            case MoveType.Rock:
+                return MoveType.Paper;
+            case MoveType.Paper:
+                return MoveType.Scissors;
+            default:
+                throw new ArgumentOutOfRangeException("move type not recognised");
+        }
+    }
+
+    private MoveType GetLoseShape(MoveType them)
+    {
+        switch (them)
+        {
+            case MoveType.Scissors:
+                return MoveType.Paper;
+            case MoveType.Rock:
+                return MoveType.Scissors;
+            case MoveType.Paper:
+                return MoveType.Rock;
+            default:
+                throw new ArgumentOutOfRangeException("move type not recognised");
+        }
     }
 }
 
-public record Round(MoveType Them, MoveType Me);
+public record Round(int Them, int Me);
+
+public record RoundPart1(MoveType Them, MoveType Me);
+
+public record RoundPart2(MoveType Them, Outcome Me);
 
 public enum MoveType
 {
     Rock = 0,
     Paper = 1,
     Scissors = 2
+}
+
+public enum Outcome
+{
+    Lose = 0,
+    Draw = 1,
+    Win = 2
 }
