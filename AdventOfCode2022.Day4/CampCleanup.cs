b78cf718b4b44c419d40189e38eeb00ff0f7ae8f
KaterynaKateryna <tok.ar.tok@gmail.com>
2022-12-04 17:48:29 +0100

    day 4 part 1

---
+++ AdventOfCode2022.Day4/CampCleanup.cs
@@ -0,0 +1,40 @@
+﻿namespace AdventOfCode2022.Day4;
+
+public class CampCleanup
+{
+    public async Task<ElfPair[]> GetInput()
+    {
+        string[] input = await File.ReadAllLinesAsync("input.txt");
+        return input.Select(i => ElfPair.Parse(i)).ToArray();
+    }
+
+    public int CountFullyContainingPairs(ElfPair[] input)
+    {
+        return input.Count(pair => 
+            FullyContains(pair.First, pair.Second) || FullyContains(pair.Second, pair.First)
+        );
+    }
+
+    private bool FullyContains(Assignment first, Assignment second)
+    {
+        return first.Start <= second.Start && first.End >= second.End;
+    }
+}
+
+public record class ElfPair(Assignment First, Assignment Second)
+{
+    public static ElfPair Parse(string input)
+    {
+        string[] elves = input.Split(',');
+        return new ElfPair(Assignment.Parse(elves[0]), Assignment.Parse(elves[1]));
+    }
+}
+
+public record Assignment(int Start, int End)
+{
+    public static Assignment Parse(string input)
+    {
+        string[] sections = input.Split('-');
+        return new Assignment(int.Parse(sections[0]), int.Parse(sections[1]));
+    }
+}
---
+++ AdventOfCode2022.Day4/Program.cs
@@ -0,0 +1,8 @@
+﻿using AdventOfCode2022.Day4;
+
+CampCleanup campCleanup = new CampCleanup();
+var input = await campCleanup.GetInput();
+
+// part1
+int count = campCleanup.CountFullyContainingPairs(input);
+Console.WriteLine(count);
