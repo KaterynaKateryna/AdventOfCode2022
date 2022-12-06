923d85a7e226795e5146d201e859d27c6402b797
KaterynaKateryna <tok.ar.tok@gmail.com>
2022-12-06 21:08:37 +0100

    day 6 part 1

---
+++ AdventOfCode2022.Day6/Program.cs
@@ -0,0 +1,8 @@
+﻿using AdventOfCode2022.Day6;
+
+TuningTrouble tuningTrouble = new TuningTrouble();
+string input = await tuningTrouble.GetInput();
+
+// part 1
+int answer = tuningTrouble.GetCharsForTheFirstStartOfPacketMarker(input);
+Console.WriteLine(answer);
---
+++ AdventOfCode2022.Day6/TuningTrouble.cs
@@ -0,0 +1,23 @@
+﻿namespace AdventOfCode2022.Day6;
+
+public class TuningTrouble
+{
+    public Task<string> GetInput() => File.ReadAllTextAsync("input.txt");
+
+    public int GetCharsForTheFirstStartOfPacketMarker(string input)
+    {
+        for (int i = 3; i < input.Length; ++i)
+        {
+            HashSet<char> temp = new HashSet<char>(4);
+            temp.Add(input[i]);
+            temp.Add(input[i-1]);
+            temp.Add(input[i-2]);
+            temp.Add(input[i-3]);
+            if (temp.Count == 4)
+            {
+                return i + 1;
+            }
+        }
+        throw new InvalidDataException("Start-of=packet marker not found");
+    }
+}
