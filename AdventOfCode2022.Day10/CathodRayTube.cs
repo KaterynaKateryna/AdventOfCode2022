254edcf7c4d56d5e3e9c00e124c95fd9beaf56ee
KaterynaKateryna <tok.ar.tok@gmail.com>
2022-12-10 20:20:54 +0100

    day 10 part 1

---
+++ AdventOfCode2022.Day10/CathodRayTube.cs
@@ -0,0 +1,47 @@
+﻿namespace AdventOfCode2022.Day10;
+
+public class CathodRayTube
+{
+    public Task<string[]> GetInput() => File.ReadAllLinesAsync("input.txt");
+
+    public long GetSumOfSignalStrengths(string[] commands)
+    {
+        int currentCycle = 0;
+        long x = 1;
+        long sum = 0;
+        foreach(string command in commands)
+        {
+            if (command == "noop")
+            {
+                (sum, currentCycle) = Cycle(currentCycle, sum, x);
+            }
+            else 
+            {
+                int arg = int.Parse(command.Split(' ')[1]);
+
+                (sum, currentCycle) = Cycle(currentCycle, sum, x); 
+                (sum, currentCycle) = Cycle(currentCycle, sum, x);
+
+                x += arg;
+            }
+        }
+        return sum;
+    }
+
+    private (long sum, int currentCycle) Cycle(int currentCycle, long sum, long x)
+    {
+        currentCycle++;
+        if (
+            currentCycle == 20
+            || currentCycle == 60
+            || currentCycle == 100
+            || currentCycle == 140
+            || currentCycle == 180
+            || currentCycle == 220
+        )
+        {
+            sum += currentCycle * x;
+        }
+        return (sum, currentCycle);
+    }
+}
---
+++ AdventOfCode2022.Day10/Program.cs
@@ -0,0 +1,8 @@
+﻿using AdventOfCode2022.Day10;
+
+CathodRayTube cathodRayTube = new CathodRayTube();
+string[] input = await cathodRayTube.GetInput();
+
+// part 1
+long sum = cathodRayTube.GetSumOfSignalStrengths(input);
+Console.WriteLine(sum);
